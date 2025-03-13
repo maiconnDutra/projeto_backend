14ce30f60667b713c21d5446f0fc8a0de654905c
maiconnDutra <unifacul0@gmail.com>
2025-03-13 20:47:54 -0300

    docs: Criando banco de dados mysql

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todos.views import todo_list_old
+from todos.views import todo_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', todo_list_old),
+    path('', todo_list),
 ]
--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 from todos.models import Todo
 
 # Create your views here.
-def todo_list_old(request):
-    nome = 'Rodrigo'
-    alunos = ['1. Ana', '2. José', '3. Bia']
-    return render(request, "todos/todo_list.html", {"nome" : nome, "lista_alunos" : alunos})
 
 
 def todo_list(request):
