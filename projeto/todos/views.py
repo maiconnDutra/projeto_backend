22f7dab1c14113a3a7d7ee46a48814dfaa509433
maiconnDutra <unifacul0@gmail.com>
2025-03-13 21:11:39 -0300

    docs: Adicionando todo_form e melhorando o formulario.

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
 from todos.models import Todo
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"];
+    success_url = reverse_lazy("todo_list")
