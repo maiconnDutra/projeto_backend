f1e880e9898a9be1481ee5cc7192a98a81f41712
maiconnDutra <unifacul0@gmail.com>
2025-05-15 20:04:02 -0300

    criando o complete

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from todos.models import Todo
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.shortcuts import get_object_or_404, redirect
+from datetime import date
 
 from django.urls import reverse_lazy
 
@@ -23,3 +25,11 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
